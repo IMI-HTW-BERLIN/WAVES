bc6cf6934fdff68b21f199ee1d7bdf5f4c83dd55
davidpgl <panagiotopulos.dtt@gmail.com>
2019-10-21 11:50:29 +0200

    Added Enemy class

--- Assets/Scripts/Entities/Enemy.cs
+++ Assets/Scripts/Entities/Enemy.cs
@@ -1,10 +1,23 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Entities
 {
     public class Enemy : Entity
     {
-        private Vector2 _target;
+        [SerializeField] private Rigidbody2D rigid;
+        private Vector3 _target;
+
+        private void FixedUpdate() => MoveToTarget();
+
+        public void SetTarget(Vector2 target) => _target = target;
+
+        private void MoveToTarget()
+        {
+            Vector3 position = transform.position;
+            Vector3 direction = (_target - position).normalized;
+            rigid.MovePosition(position + Time.deltaTime * baseMovementSpeed * direction);
+        }
         
     }
 }
