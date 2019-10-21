a1bd629a8b03b41004b4331ac0b32bebda9b35c8
Luis Hankel <kaes3kuch3n@gmail.com>
2019-10-21 12:53:22 +0200

    Added game over screen

---
+++ Assets/Scripts/UI/GameOver.cs
@@ -0,0 +1,33 @@
+﻿using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class GameOver : MonoBehaviour
+    {
+        [SerializeField]
+        private TextMeshProUGUI scoreLabel;
+        [SerializeField]
+        private Button retryButton;
+        [SerializeField]
+        private Button quitButton;
+
+        public void Show()
+        {
+            scoreLabel.SetText(GameManager.Score.ToString());
+            gameObject.SetActive(true);
+        }
+
+        private void Hide() => gameObject.SetActive(false);
+
+        private void Start()
+        {
+            retryButton.onClick.AddListener(GameManager.ResetGame);
+            retryButton.onClick.AddListener(Hide);
+            quitButton.onClick.AddListener(Application.Quit);
+            quitButton.onClick.AddListener(() => Debug.Log("Quitting..."));
+        }
+    }
+}
