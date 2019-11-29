ca0c4928bae2d5c4034bb32fa9eb9c3eda5d22df
David <panagiotopulos.d@gmail.com>
2019-11-29 22:03:37 +0100

    [New UI]

--- Assets/Scripts/UI/UpgradeMenu.cs
+++ Assets/Scripts/UI/UpgradeMenu.cs
@@ -6,24 +6,14 @@ namespace UI
 {
     public class UpgradeMenu : MonoBehaviour
     {
-        [SerializeField] private Vector2 offset;
-
         private Building _selectedBuilding;
-
-        private void Update()
-        {
-            if (!_selectedBuilding) return;
-            transform.position = _selectedBuilding.transform.position + Vector3.right * offset.x + Vector3.up * offset.y;
-        }
 
         public void ShowForBuilding(Building building)
         {
             // If menu is already shown for this building, return
-            if (_selectedBuilding == building) return;
+            if (_selectedBuilding == building || building is Base) return;
             // Store selected building
             _selectedBuilding = building;
-            // Set position relative to building
-            Update();
             // Show menu
             gameObject.SetActive(true);
         }
